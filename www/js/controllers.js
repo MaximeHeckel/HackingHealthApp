7073f350e7343c24a864159b58bf7ed84c5e979d
Gab-Metzger <gab.metzger@gmail.com>
2015-03-21 12:12:28 +0100

    Add line Chart in Morpho view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,6 +6,39 @@ angular.module('starter.controllers', [])
 
 .controller('MorphoCtrl', function($scope, morphoData){
   console.log(morphoData.all());
+  var legendData = [];
+  for (var i = 0; i <= 36; i += 3) {
+    legendData.push(i)
+  }
+
+  $scope.options = {
+    showTooltips: false,
+    scaleShowGridLines : true,
+    responsive: true,
+    pointDot: false,
+    datasetStrokeWidth : 1,
+  }
+
+  $scope.chart = {
+    labels : legendData,
+    datasets : [
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "rgba(151,187,205,1)",
+            data : [2.414,4.268,5.735,6.887,7.795,8.515,9.095,9.575,9.984,10.348,10.685,11.010,11.334],
+        },
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "rgba(151,187,205,0.3)",
+            data : [3.399,5.540,7.207,8.500,9.514,10.322,10.986,11.551,12.054,12.520,12.969,13.414,13.865]
+        },
+        {
+            fillColor : "rgba(151,187,205,0)",
+            strokeColor : "rgba(151,187,205,1)",
+            data : [4.254,6.867,8.882,10.440,11.669,12.671,13.524,14.286,15.001,15.700,16.403,17.124,17.870]
+        }
+    ]
+};
 })
 
 .controller('PlaylistsCtrl', function($scope) {
