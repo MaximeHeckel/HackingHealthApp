3e8caccfdebe37bd664ceebc2d7bb689c41c23ee
Maxime Heckel <heckelmaxime@gmail.com>
2015-03-21 09:51:46 +0100

    add basic data service to app

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers','starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -53,6 +53,16 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         'menuContent': {
           templateUrl: "templates/playlists.html",
           controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+    .state('app.morpho', {
+      url: "/morpho",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/morpho.html",
+          controller: 'MorphoCtrl'
         }
       }
     })
---
+++ www/js/services.js
@@ -0,0 +1,30 @@
+angular.module('starter.services', [])
+
+.factory('morphoData', function(){
+  var morphoData = [
+  {
+    id: 0,
+    height: 51,
+    weight: 3.500,
+    date: new Date('12','03','2014')
+  },
+  {
+    id: 1,
+    height: 51.5,
+    weight: 3.550,
+    date: new Date('13','03','2014')
+  },
+  {
+    id: 2,
+    height: 53.5,
+    weight: 3.600,
+    date: new Date('13','03','2014')
+  }
+    ];
+
+    return {
+      all: function(){
+        return morphoData;
+      }
+    }
+});
