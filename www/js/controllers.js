5078a49ee5e2081e3ccaacc98f4076308b8c28d0
Gab-Metzger <gab.metzger@gmail.com>
2015-03-22 12:04:59 +0100

    Design morpho view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -90,7 +90,10 @@ angular.module('starter.controllers', [])
     scaleShowGridLines : true,
     responsive: true,
     pointDot: false,
-    datasetStrokeWidth : 1,
+    datasetStrokeWidth : 4,
+    scaleShowHorizontalLines: false,
+    scaleShowVerticalLines: false,
+    showScale: true,
   }
 
   $scope.chart = {
@@ -98,22 +101,22 @@ angular.module('starter.controllers', [])
     datasets : [
         {
             fillColor : "rgba(151,187,205,0)",
-            strokeColor : "rgba(151,187,205,1)",
+            strokeColor : "rgba(27,173,162,0.45)", // down
             data : [2.414,2.550,2.683,2.821,2.949,3.074,3.198,3.320,3.461,3.579,3.695,3.809,3.922],
         },
         {
             fillColor : "rgba(151,187,205,0)",
-            strokeColor : "rgba(151,187,205,0.3)",
+            strokeColor : "rgba(27,173,162,0.2)", //middle
             data : [3.399,3.557,3.712,3.872,4.019,4.165,4.307,4.448,4.613,4.748,4.882,5.013,5.142]
         },
         {
             fillColor : "rgba(151,187,205,0)",
-            strokeColor : "rgba(151,187,205,1)",
+            strokeColor : "rgba(27,173,162,0.45)", //top
             data : [4.254,4.449,4.638,4.834,5.015,5.192,5.366,5.538,5.740,5.905,6.067,6.227,6.383]
         },
         {
             fillColor : "rgba(213,40,40,0)",
-            strokeColor : "rgba(213,40,40,1)",
+            strokeColor : "rgba(27,173,162,1)",
             data : [3.717,3.887,4.053,4.225,4.383,4.539,4.692,4.692,4.613]
         }
     ]
